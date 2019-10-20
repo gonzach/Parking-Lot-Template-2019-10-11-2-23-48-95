4725234caee5fe4a9839128d470d293922faf247
CHRISTINE GONZALES (SAPE-MIS-ISD-OOCLL/MNL) <GONZACH@OOCL.COM>
2019-10-20 10:37:28 +0800

    - fix build.gradle - add parkinglot class

---
+++ src/main/java/com/thoughtworks/parking_lot/core/Parkinglot.java
@@ -0,0 +1,34 @@
+package com.thoughtworks.parking_lot.core;
+
+class ParkingLots {
+
+    private String name;
+
+    private int capacity;
+
+    private String location;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
